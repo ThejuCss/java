948157a4e0f2a795558c126c42020c6da33fe823
Thejaswini VijayaKumar <48344390+ThejuCss@users.noreply.github.com>
2019-07-04 07:38:43 +0530

    Update MyClass.java

--- MyClass.java
+++ MyClass.java
@@ -1,30 +1,16 @@
 import java.util.*;
-
 public class MyClass 
 {
-
-  
   public static void main(String args[])
-  
   {
-    
     Scanner sc=new Scanner(System.in);
-    
     System.out.println("enter string");
-    
     String s=sc.nextLine();
-    
-    char[] c = s.toCharArray(); 
-    
+    char[] c = s.toCharArray();
      for (int i = c.length-1; i>=0; i--)
-   
      { 
-      
-       System.out.print(c[i]); 
-    
+       System.out.print(c[i]);
      }
-  
-   }
-
+  }
 } 
 
