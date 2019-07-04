b8af8877c9393d69dd998cc8963973fd622b70e8
Thejaswini VijayaKumar <48344390+ThejuCss@users.noreply.github.com>
2019-07-04 07:36:23 +0530

    Add files via upload

---
+++ MyClass.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+public class MyClass 
+{
+
+  
+  public static void main(String args[])
+  
+  {
+    
+    Scanner sc=new Scanner(System.in);
+    
+    System.out.println("enter string");
+    
+    String s=sc.nextLine();
+    
+    char[] c = s.toCharArray(); 
+    
+     for (int i = c.length-1; i>=0; i--)
+   
+     { 
+      
+       System.out.print(c[i]); 
+    
+     }
+  
+   }
+
+} 
+
